4032868e77c1070bf4b205ebd350691f9c015f7d
dhirajberi <dhiraj.b@yudiz.com>
2022-07-28 16:41:06 +0530

    static files configurations

---
+++ projects/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import redirect, render
+from .models import Project
+from .forms import ProjectForm
+
+def projects(request):
+    projects = Project.objects.all()
+    context = {'projects': projects}
+    return render(request, 'projects.html', context)
+
+def project(request, pk):
+    project = Project.objects.get(id=pk)
+    tags = project.tags.all()
+    context = {'project': project, 'tags': tags}
+    return render(request, 'single-project.html', context)
+
+def createProject(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    form = ProjectForm()
+    context = {'form': form}
+    return render(request, 'project_form.html', context)
+
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+    form = ProjectForm(instance = project)
+    context = {'form': form}
+    return render(request, 'project_form.html', context)
+
+def deleteProject(request, pk):
+    project = Project.objects.get(id=pk)
+    project.delete()
+    return redirect('projects')
