e528832a4d458baca48b467e93f7f8f8c30ce55a
dhirajberi <dhiraj.b@yudiz.com>
2022-07-28 17:55:58 +0530

    theme installation

--- projects/forms.py
+++ projects/forms.py
@@ -1,7 +1,18 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets
 from .models import Project
+from django import forms
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         exclude = ('vote_ratio', 'vote_total')
+
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
--- projects/views.py
+++ projects/views.py
@@ -5,13 +5,13 @@ from .forms import ProjectForm
 def projects(request):
     projects = Project.objects.all()
     context = {'projects': projects}
-    return render(request, 'projects/projects.html', context)
+    return render(request, 'projects.html', context)
 
 def project(request, pk):
     project = Project.objects.get(id=pk)
     tags = project.tags.all()
     context = {'project': project, 'tags': tags}
-    return render(request, 'projects/single-project.html', context)
+    return render(request, 'single-project.html', context)
 
 def createProject(request):
     if request.method == 'POST':
@@ -22,7 +22,7 @@ def createProject(request):
 
     form = ProjectForm()
     context = {'form': form}
-    return render(request, 'projects/project_form.html', context)
+    return render(request, 'form-template.html', context)
 
 def updateProject(request, pk):
     project = Project.objects.get(id=pk)
